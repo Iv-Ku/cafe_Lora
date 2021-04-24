d890b8e3d67ae30a789307f2938ed61b83b61ee1
Ivana Kubičková <ivkuku@gmail.com>
2021-04-24 13:42:33 +0200

    menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
 
 console.log('funguju!');
+
+const menuElm = document.querySelector('#nav-btn');
+
+menuElm.addEventListener('click', (event) => {
+  console.log('Stisknuto');
+  const navigace = document.querySelector('nav');
+  navigace.classList.toggle('nav-closed');
+});
+
+const polozkyMenuElm = document.querySelector('.navigation a');
+polozkyMenuElm.forEach((element) => {
+  console.log('vypsat element', element);
+  element.addEventListener('click', () => {
+    console.log('stisknutí položky');
+    const navElm = document.querySelector('nav');
+    navElm.classList.toggle('nav-closed');
+  });
+});
